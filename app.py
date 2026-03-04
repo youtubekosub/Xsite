755f10233816d6d70234802f9818225308a08b5f
youtubekosub <kappayosi@gmail.com>
2026-03-04 17:30:22 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,66 @@
+import hmac
+import hashlib
+import base64
+import time
+import os
+from flask import Flask, request, redirect, abort, render_template
+
+app = Flask(__name__)
+
+# Renderの環境変数から取得。なければデフォルト。
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default-key-3.14159').encode()
+LINK_DURATION = 600 # 10分
+
+def generate_secure_url(target_url):
+    expires = str(int(time.time()) + LINK_DURATION)
+    b64_url = base64.urlsafe_b64encode(target_url.encode()).decode()
+    data_to_sign = f"{b64_url}{expires}".encode()
+    signature = hmac.new(SECRET_KEY, data_to_sign, hashlib.sha256).hexdigest()[:16]
+    return f"/goto?u={b64_url}&e={expires}&s={signature}"
+
+@app.route('/')
+def index():
+    query = request.args.get('q', '')
+    results = []
+    if query:
+        # デモ用データ。実際にはここで検索APIを叩く。
+        mock_data = [
+            {"title": f"{query} - 匿名検索結果 1", "url": f"https://www.google.com/search?q={query}", "desc": "HMAC署名によって保護された、プライバシー重視の検索結果です。"},
+            {"title": "Wikipedia: プライバシーについて", "url": "https://ja.wikipedia.org/wiki/プライバシー", "desc": "個人の私生活上の事柄をみだりに公開されない法的権利についての解説。"},
+            {"title": "GitHub: Open Source Projects", "url": "https://github.com", "desc": "ソースコードのホスティングとプライベートな開発が可能なプラットフォーム。"}
+        ]
+        for res in mock_data:
+            res['proxy_url'] = generate_secure_url(res['url'])
+            results.append(res)
+    
+    # templates/index.html を読み込む
+    return render_template('index.html', query=query, results=results)
+
+@app.route('/goto')
+def goto():
+    b64_url = request.args.get('u')
+    expires = request.args.get('e')
+    signature = request.args.get('s')
+
+    if not all([b64_url, expires, signature]):
+        abort(400)
+
+    data_to_verify = f"{b64_url}{expires}".encode()
+    expected_sig = hmac.new(SECRET_KEY, data_to_verify, hashlib.sha256).hexdigest()[:16]
+    
+    if not hmac.compare_digest(signature, expected_sig) or int(time.time()) > int(expires):
+        return "Invalid or Expired Link", 403
+
+    try:
+        target_url = base64.urlsafe_b64decode(b64_url).decode()
+    except:
+        abort(400)
+
+    response = redirect(target_url)
+    response.headers['Referrer-Policy'] = 'no-referrer'
+    return response
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
